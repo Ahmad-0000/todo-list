3b7b03509000dbd16586080f0173260f38d2d488
Ahmad <ahmad.new.m.v@gmial.com>
2025-06-14 14:38:33 +0200

    Configure to use MySQL

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': getenv('TODOAPP_DB_HOST'),
+        'HOST': getenv('TODOAPP_DB_HOST'),
+        'USER': getenv('TODOAPP_DB_USER'),
+        'PASSWORD': getenv('TODOAPP_DB_PAWD'),
     }
 }
 
