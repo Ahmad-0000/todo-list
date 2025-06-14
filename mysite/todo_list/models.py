a4622c73d9d6075fd675e8e0f3075674282513db
Ahmad <ahmad.new.m.v@gmial.com>
2025-06-14 16:23:14 +0200

    Create the task model

--- mysite/todo_list/models.py
+++ mysite/todo_list/models.py
@@ -13,3 +13,20 @@ class User(models.Model):
         Return the user name
         """
         return self.name
+
+class Task(models.Model):
+    """
+    Represent a task
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    add_date = models.DateTimeField('addition_date')
+    deadline = models.DateTimeField()
+    done_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        """
+        Return the task's title
+        """
+        return self.title
