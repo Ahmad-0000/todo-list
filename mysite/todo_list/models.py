0d5aac822193dda1ca7a6879f320ae42e751e46d
Ahmad <ahmad.new.m.v@gmial.com>
2025-06-14 16:12:54 +0200

    Create the user model

--- mysite/todo_list/models.py
+++ mysite/todo_list/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """
+    Represent an user
+    """
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        """
+        Return the user name
+        """
+        return self.name
