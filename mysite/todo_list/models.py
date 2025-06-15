85fa2c0527faafae98cf26d3a7f4e43dfc069037
Ahmad <ahmad.new.m.v@gmail.com>
2025-06-15 19:42:11 +0200

    Ensure email is unique

--- mysite/todo_list/models.py
+++ mysite/todo_list/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     Represent an user
     """
     name = models.CharField(max_length=20)
-    email = models.EmailField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
     password = models.CharField(max_length=100)
 
     def __str__(self):
