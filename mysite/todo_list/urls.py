e37ef74d4b213c09a332141b5ab1cbe4a13ab84e
Ahmad <ahmad.new.m.v@gmail.com>
2025-06-16 21:19:09 +0200

    Link index view function with url

--- mysite/todo_list/urls.py
+++ mysite/todo_list/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from . import views
 
 app_name = 'todo_list'
 urlpatterns = [
+    path("/", views.index, name='index'),
     path("accounts/", include('django.contrib.auth.urls')),
 ]
