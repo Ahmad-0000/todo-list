8295f0e350bf1d1f51a496f46913d7d04e09fb61
Ahmad <ahmad.new.m.v@gmail.com>
2025-06-16 19:52:58 +0200

    Comment

--- mysite/todo_list/models.py
+++ mysite/todo_list/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     """
     name = models.CharField(max_length=20)
     email = models.EmailField(max_length=50, unique=True)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=100) # Too long because of encryption
 
     def __str__(self):
         """
