4332cec6538be84174490265b1ec2b9758e4860e
Ahmad <ahmad.new.m.v@gmail.com>
2025-06-17 23:51:38 +0200

    Test TaskForm fields

--- mysite/todo_list/tests/forms/test_tasks.py
+++ mysite/todo_list/tests/forms/test_tasks.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.forms import ModelForm
+from django.forms import ModelForm, CharField, DateTimeField
 from ...forms import TaskForm
 
 class TestTaskForm(TestCase):
@@ -11,3 +11,18 @@ class TestTaskForm(TestCase):
         TaskForm is a subclass of ModelForm
         """
         self.assertIsInstance(TaskForm(), ModelForm)
+
+    def test_task_form_fields(self):
+        """
+        TaskForm fields are title, description and
+        deadline, with types CharField, CharField and
+        DateTimeField respectively.
+        """
+        tf = TaskForm()
+        self.assertEqual(len(tf.fields.keys()), 3)
+        self.assertIn('title', tf.fields.keys())
+        self.assertIn('description', tf.fields.keys())
+        self.assertIn('deadline', tf.fields.keys())
+        self.assertIsInstance(tf.fields['title'], CharField)
+        self.assertIsInstance(tf.fields['description'], CharField)
+        self.assertIsInstance(tf.fields['deadline'], DateTimeField)
